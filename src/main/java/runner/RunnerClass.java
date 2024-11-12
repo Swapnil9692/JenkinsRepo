840a3845a46f71b1003aa65a3876ee1661de3521
Swaphoenix96 <Swaphoenix96@gail.com>
2024-11-12 09:32:14 +0530

    HTML report Code added

--- src/main/java/runner/RunnerClass.java
+++ src/main/java/runner/RunnerClass.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 (
 	features= {"src\\test\\resources\\login.feature"},
 	glue = {"stepDefination"},
-	plugin= {"pretty"},
+	plugin= {"pretty","html:target/cucumber-reports/exectuion.html"},
 	publish = true
 )
 
